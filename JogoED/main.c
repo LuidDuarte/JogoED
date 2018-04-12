a0ee18d6c6ed4169cbbfe9de81cb064f8998974a
Luid Duarte <luidduarte@yahoo.com.br>
2018-04-11 23:23:00 -0300

    personagem Earl adicionado. personagem.h e personagem.c criados

--- JogoED/main.c
+++ JogoED/main.c
@@ -8,54 +8,13 @@
 #include <allegro5\allegro_primitives.h>
 #include <allegro5\keyboard.h>
 #include <allegro5\timer.h>
+#include "personagem.h"
+
 
 
 #define false 0 //caso o compilador seja antigo e ainda não tenha bool.
 #define true 1
 #define FPS 60
-
-void carregaImagens(ALLEGRO_BITMAP *toe[]) {
-	
-	/*Load animação Toejam
-	0 a 2 -> Parado
-	3 a 8 -> Subindo
-	9 -> 14 -> Direita
-	15 a 20 -> Descendo
-	21 a 26 -> Esquerda
-	*/
-
-	toe[0] = al_load_bitmap("..\\imagens\\toejam\\toe0.png");
-	toe[1] = al_load_bitmap("..\\imagens\\toejam\\toe1.png");
-	toe[2] = al_load_bitmap("..\\imagens\\toejam\\toe2.png");
-	toe[3] = al_load_bitmap("..\\imagens\\toejam\\toe3.png");
-	toe[4] = al_load_bitmap("..\\imagens\\toejam\\toe4.png");
-	toe[5] = al_load_bitmap("..\\imagens\\toejam\\toe5.png");
-	toe[6] = al_load_bitmap("..\\imagens\\toejam\\toe6.png");
-	toe[7] = al_load_bitmap("..\\imagens\\toejam\\toe7.png");
-	toe[8] = al_load_bitmap("..\\imagens\\toejam\\toe8.png");
-	toe[9] = al_load_bitmap("..\\imagens\\toejam\\toe9.png");
-	toe[10] = al_load_bitmap("..\\imagens\\toejam\\toe10.png");
-	toe[11] = al_load_bitmap("..\\imagens\\toejam\\toe11.png");
-	toe[12] = al_load_bitmap("..\\imagens\\toejam\\toe12.png");
-	toe[13] = al_load_bitmap("..\\imagens\\toejam\\toe13.png");
-	toe[14] = al_load_bitmap("..\\imagens\\toejam\\toe14.png");
-	toe[15] = al_load_bitmap("..\\imagens\\toejam\\toe15.png");
-	toe[16] = al_load_bitmap("..\\imagens\\toejam\\toe16.png");
-	toe[17] = al_load_bitmap("..\\imagens\\toejam\\toe17.png");
-	toe[18] = al_load_bitmap("..\\imagens\\toejam\\toe18.png");
-	toe[19] = al_load_bitmap("..\\imagens\\toejam\\toe19.png");
-	toe[20] = al_load_bitmap("..\\imagens\\toejam\\toe20.png");
-	toe[21] = al_load_bitmap("..\\imagens\\toejam\\toe21.png");
-	toe[22] = al_load_bitmap("..\\imagens\\toejam\\toe22.png");
-	toe[23] = al_load_bitmap("..\\imagens\\toejam\\toe23.png");
-	toe[24] = al_load_bitmap("..\\imagens\\toejam\\toe24.png");
-	toe[25] = al_load_bitmap("..\\imagens\\toejam\\toe25.png");
-	toe[26] = al_load_bitmap("..\\imagens\\toejam\\toe26.png");
-
-
-
-	
-}
 
 void mapa(int);
 
@@ -63,9 +22,10 @@ void mapa(int);
 int map1[16][30];
 
 
-int main() { 
-	int pwx = 12;
-	int pwy = 5;
+int main() { 	
+	personagem p;
+	p.x = 12;
+	p.y = 5;
 	int xImagem = 0;
 	int yImagem = 0;
 	int done = false;
@@ -77,10 +37,10 @@ int main() {
 	al_install_keyboard();
 
 	//Variáveis Allegro
-	ALLEGRO_DISPLAY* janela = al_create_display(640, 400);
+	ALLEGRO_DISPLAY* janela = al_create_display(800, 600);
 	ALLEGRO_BITMAP *buffer = NULL;
 	ALLEGRO_BITMAP *imagem = NULL;
-	ALLEGRO_BITMAP *toe[27];
+	ALLEGRO_BITMAP *moldura = NULL;
 	ALLEGRO_TIMER *timer = al_create_timer(1.0 / FPS);
 	ALLEGRO_KEYBOARD_STATE keyState;
 	ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();
@@ -101,7 +61,8 @@ int main() {
 	ALLEGRO_EVENT events;
 	mapa(1);
 	imagem = al_load_bitmap("..\\imagens\\1500x800fase1.png");
-	carregaImagens(toe);
+	moldura = al_load_bitmap("..\\imagens\\moldura.png");
+	carregaEarl(&p);
 
 	
 
@@ -110,8 +71,9 @@ int main() {
 	while (!done) {
 		tempo1 = al_get_time();
 								//Essas subtrações são feitas para que o personagem fique ainda no meio da tela, e em cima do continente. 
-		al_draw_bitmap(imagem, xImagem-280, yImagem-50, 300,200);
-		al_draw_bitmap(toe[animacao], 320, 200, 0);
+		al_draw_bitmap(imagem, xImagem-280, yImagem-50, 0);
+		al_draw_bitmap(p.personagem[animacao], 320, 200, 0);
+		al_draw_bitmap(moldura, 0, 0, 0);
 		al_flip_display();
 		al_wait_for_event(event_queue, &events);
 
@@ -126,8 +88,8 @@ int main() {
 					animacao++;
 				else if (animacao >= 14 || animacao < 9)
 					animacao = 9;
-				if (map1[pwy][pwx+1] == 1) {
-					pwx++;
+				if (map1[p.y][p.x+1] == 1) {
+					p.x++;
 					xImagem -= 50;
 				}
 				break;
@@ -137,8 +99,8 @@ int main() {
 					animacao++;
 				else if (animacao >= 26 || animacao < 21)
 					animacao = 21;
-				if (map1[pwy][pwx-1] == 1) {
-					pwx--;
+				if (map1[p.y][p.x-1] == 1) {
+					p.x--;
 					xImagem += 50;
 				}
 				break;
@@ -148,8 +110,8 @@ int main() {
 					animacao++;
 				else if (animacao >= 8 || animacao < 3)
 					animacao = 3;
-				if (map1[pwy-1][pwx] == 1) {
-					pwy--;
+				if (map1[p.y-1][p.x] == 1) {
+					p.y--;
 					yImagem += 50;
 				}
 				break;
@@ -159,8 +121,8 @@ int main() {
 					animacao++;
 				else if (animacao >= 20 || animacao < 15)
 					animacao = 15;
-				if (map1[pwy+1][pwx] == 1) {
-					pwy++;
+				if (map1[p.y+1][p.x] == 1) {
+					p.y++;
 					yImagem -= 50;
 				}
 				break;
@@ -192,7 +154,7 @@ void mapa(mood) {
 /*5  */	{	0,	0,	0,	0,	0,	0,	0,	0,	0,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	0,	0,	0,	0 },
 /*6  */	{	0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	0,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	0,	0,	0,	0,	0 },
 /*7  */	{	0,	0,	0,	0,	0,	0,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	0,	0,	0,	0,	0,	0,	0,	0 },
-/*8  */	{	0,	0,	0,	0,	1,	1,	1,	1,	0,	0,	0,	0,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	0,	0,	0,	0,	0,	0,	0 },
+/*8  */	{	0,	0,	0,	1,	1,	1,	1,	1,	0,	0,	0,	0,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	0,	0,	0,	0,	0,	0,	0 },
 /*9 */	{	0,	0,	0,	0,	0,	1,	1,	1,	1,	1,	0,	0,	0,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	0,	0,	0,	0,	0 },
 /*10*/	{	0,	0,	0,	0,	0,	0,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	0,	0,	0,	0,	0 },
 /*11*/	{	0,	0,	0,	0,	0,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	1,	0,	0,	0,	0,	0,	0,	0 },
---
+++ JogoED/personagem.h
@@ -0,0 +1,16 @@
+#include <allegro5\allegro.h>
+
+#ifndef PERSONAGEM_H
+#define PERSONAGEM_H
+
+typedef struct personagem {
+	int x;
+	int y;
+	ALLEGRO_BITMAP *personagem[27];
+
+}personagem;
+
+void carregaToeJam(personagem* p);
+void carregaEarl(personagem* p);
+
+#endif
